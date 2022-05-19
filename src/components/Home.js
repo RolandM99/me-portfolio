1eed2ab0d753a9b51c27141932363d2395e85aad
rolandm99 <manfulmweze99@gmail.com>
2022-05-19 13:25:11 +0100

    Add react icons packages and customize css style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
 import { bubble as Menu } from 'react-burger-menu';
 import avatar from '../assets/images/Ellipse.png';
+import { FaAngellist } from 'react-icons/fa';
+import resume from '../assets/Resume.pdf';
+import {
+  BsInstagram,
+  BsLinkedin,
+  BsTwitter,
+  BsGithub,
+} from 'react-icons/bs';
+import { HiOutlineDocumentDownload } from 'react-icons/hi';
 
 export default class Home extends React.Component {
   showSettings (event) {
@@ -10,24 +20,69 @@ export default class Home extends React.Component {
   render () {
     return (
      <div className="home">
-       <Menu right width={ 250 }>
+       <Menu className="my-bar" right width={ "100%" }>
         <a className="menu-item" href="#home">Home</a>
         <a className="menu-item" href="#about">About</a>
         <a className="menu-item" href="#project">Projects</a>
         <a className="menu-item" href="#contact">Contact</a>
       </Menu>
+      <div className="social-media">
+        <ul className="my-social">
+          <li>
+            <a href="https://github.com/RolandM99">
+              <IconContext.Provider value={{ className: 'sidebar-link-icon' }}>
+                <BsGithub />
+              </IconContext.Provider>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <IconContext.Provider value={{ className: 'sidebar-link-icon' }}>
+                <BsInstagram />
+              </IconContext.Provider>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <IconContext.Provider value={{ className: 'sidebar-link-icon' }}>
+                <BsTwitter />
+              </IconContext.Provider>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/roland-mweze/">
+              <IconContext.Provider value={{ className: 'sidebar-link-icon' }}>
+                <BsLinkedin />
+              </IconContext.Provider>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <IconContext.Provider value={{ className: 'sidebar-link-icon' }}>
+                <FaAngellist />
+              </IconContext.Provider>
+            </a>
+          </li>
+        </ul>
+      </div>
          <div className="description">
            <img id="my-avatar" src ={avatar} alt="my avatar" />
            <div className="intro">
              <div className="text">
-               <p className="hello">Hello there, I'm </p>
-               <p className="name"><strong>Roland N. Mweze </strong></p>
+               <p className="hello">Hello there,</p>
+               <p className="name">I am <strong>Roland MWEZE</strong></p>
                <p className="role">Software Engineer</p>
              </div>
              <div className="action">
                 <a href="#contact">
-                 <button className="hire">HIRE ME</button>
+                 <button className="hire">Hire me</button>
                 </a>
+                <a href={resume} download id="resume" className="download-cv">
+                Download CV
+                <IconContext.Provider value={{ className: 'download-icon' }}>
+                  <HiOutlineDocumentDownload />
+                </IconContext.Provider>
+              </a>
              </div>
            </div>
          </div>
