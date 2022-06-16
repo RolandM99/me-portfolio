12e5862d00a1b9f958a12ac1c5ef8b75b8ce80af
rolandm99 <manfulmweze99@gmail.com>
2022-06-16 16:19:02 +0100

    Add new functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Portfolio />
+      <About />
     </div>
   );
 }
