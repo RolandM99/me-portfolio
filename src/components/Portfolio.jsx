6d1a2802510538cf5fa4bd577fd7f74e74f5de1b
rolandm99 <manfulmweze99@gmail.com>
2022-06-29 13:13:56 +0100

    Update portfolio content

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,55 +3,93 @@ import { Card } from 'react-bootstrap';
 import { IconContext } from 'react-icons';
 import { BsGithub, BsBoxArrowUpRight } from "react-icons/bs";
 import pro1 from '../assets/images/AuctionArt0.png';
+import pro2 from '../assets/images/woman.png';
+import pro3 from '../assets/images/bulga.png';
+import pro4 from '../assets/images/doc.png';
+import pro5 from '../assets/images/weather.png';
+import pro6 from '../assets/images/math.png';
 
 const projects = [{
   id: 1,
   image: pro1,
-  title: 'First Title',
-  description: 'text',
-  source: 'https://github.com/rolandm99/react-redux-capstone',
-  demo: 'https://determined-aryabhata.netlify.app/',
+  title: 'AuctionArt',
+  description: 'This website display astronomy captured picture of the Day. Users can like, see details about pictures or astronomy artwork and comment.',
+  source: 'https://github.com/RolandM99/API-based-WebApp',
+  demo: 'https://github.com/RolandM99/API-based-WebApp',
+  techno: [
+    {id: 1, stack: "JS"},
+    {id: 2, stack: "HTML"},
+    {id: 3, stack: "CSS"},
+    {id: 4, stack: "Webpack"}
+  ],
 },
 {
   id: 2,
-  image: pro1,
-  title: 'First Title',
-  description: 'text',
-  source: 'https://github.com/rolandm99/react-redux-capstone',
-  demo: 'https://determined-aryabhata.netlify.app/',
+  image: pro2,
+  title: 'Strong Women',
+  description: 'This project is based on an online website for a conference(Strong Women DRC 2021)',
+  source: 'https://github.com/RolandM99/Capstone-Conference-page',
+  demo: 'https://rolandm99.github.io/Capstone-Conference-page/',
+  techno: [
+    {id: 1, stack: "JavaScript"},
+    {id: 2, stack: "HTML"},
+    {id: 3, stack: "CSS"},
+  ],
 },
 {
   id: 3,
-  image: pro1,
-  title: 'First Title',
-  description: 'text',
-  source: 'https://github.com/rolandm99/react-redux-capstone',
-  demo: 'https://determined-aryabhata.netlify.app/',
+  image: pro3,
+  title: 'Bulga App',
+  description: 'BulgaApp is a mobile web application where you can manage your budget, so that you can see how much money you spent and on what',
+  source: 'https://github.com/RolandM99/budget-app',
+  demo: 'https://bulgabudget.herokuapp.com/',
+  techno: [
+    {id: 1, stack: "Ruby"},
+    {id: 2, stack: "Ruby on Rails"},
+    {id: 3, stack: "CSS"},
+  ],
 },
 {
   id: 4,
-  image: pro1,
-  title: 'First Title',
-  description: 'text',
+  image: pro4,
+  title: 'ARoZac Care',
+  description: 'a full-stack web application that can allow users to create an account, book an appointment with a doctor.',
   source: 'https://github.com/rolandm99/react-redux-capstone',
   demo: 'https://determined-aryabhata.netlify.app/',
+  techno: [
+    {id: 1, stack: "ReactJs"},
+    {id: 2, stack: "RoR"},
+    {id: 3, stack: "CSS"},
+    {id: 4, stack: "Redux"}
+  ],
 },
 {
   id: 5,
-  image: pro1,
-  title: 'First Title',
-  description: 'text',
-  source: 'https://github.com/rolandm99/react-redux-capstone',
-  demo: 'https://determined-aryabhata.netlify.app/',
+  image: pro5,
+  title: 'WeatherApp',
+  description: 'WeatherApp is a mobile web application that provides numeric data about weather in my country, DRC.',
+  source: 'https://github.com/RolandM99/weather-App',
+  demo: 'https://drcweather-app.herokuapp.com/',
+  techno: [
+    {id: 1, stack: "ReactJs"},
+    {id: 2, stack: "Redux"},
+    {id: 3, stack: "CSS"},
+  ],
 },
 {
   id: 6,
-  image: pro1,
-  title: 'First Title',
-  description: 'text',
-  source: 'https://github.com/rolandm99/react-redux-capstone',
-  demo: 'https://determined-aryabhata.netlify.app/',
-}]
+  image: pro6,
+  title: 'Math Magician',
+  description: 'A website for all fans of mathematics that allows users to make simple calculations and read a random math-related quote',
+  source: 'https://github.com/RolandM99/math-magicians',
+  demo: 'https://math-magic.netlify.app/',
+  techno: [
+    {id: 1, stack: "React"},
+    {id: 2, stack: "JavaScript"},
+    {id: 3, stack: "CSS"},
+  ],
+}
+]
 
 export default function Portfolio() {
  return (
@@ -61,7 +99,7 @@ export default function Portfolio() {
          <h1 className="pro-title">My Portfolio<hr className="line2"></hr></h1>
          <p className="txt">Here are some of my projects:</p>
          <div className="card-container">
-         {projects.map(({ id, image, title, description, source, demo }) => {
+         {projects.map(({ id, image, title, description, source, demo, techno }) => {
            return (
             <div className="card-pro">
               <Card key={id}>
@@ -71,9 +109,9 @@ export default function Portfolio() {
                   <Card.Text style={{ color: 'white' }}>
                     {description}
                   </Card.Text>
-                  <span className="span">JavaScript</span>
-                  <span className="span">HTML</span>
-                  <span className="span">CSS</span>
+                  <div>{techno.map((tec) => (
+                    <span key={tec.id} className="span" >{tec.stack}</span>
+                  ))}</div>
                   <div className="link-to">
                     <a href={source} className="link-out">
                       Source Code {" "}
